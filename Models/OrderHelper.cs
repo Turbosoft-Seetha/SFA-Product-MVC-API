c5d7174c47d098765f24609319ef11b815365cc2
ShamilaSali <FizzaSaleel@123>
2024-10-18 12:02:18 +0530

    freesample

--- Models/OrderHelper.cs
+++ Models/OrderHelper.cs
@@ -288,5 +288,45 @@ namespace MVC_API.Models
 
 
     }
-   
+    public class PostfreeSampleData
+    {
+        public string OrderID { get; set; }
+        public string RotId { get; set; }
+        public string CusId { get; set; }
+        public string UserId { get; set; }
+        public string UdpId { get; set; }
+        public string JSONString { get; set; }
+
+    }
+    public class PostFreeSamplelDetData
+    {
+        public string PrdID { get; set; }
+        public string HigherQty { get; set; }
+        public string HigherUOM { get; set; }
+        public string LowerQty { get; set; }
+        public string LowerUOM { get; set; }
+    }
+    public class GetFreeSampleApprovalStatus
+    {
+
+        public string Res { get; set; }
+        public string Title { get; set; }
+        public string Descr { get; set; }
+        public string ReturnId { get; set; }
+    }
+    public class GetFreeHeaderStatus
+    {
+        public string RotId { get; set; }
+        public string CusId { get; set; }
+        public string ReturnId { get; set; }
+    }
+    public class GetDetFreeSapmpleApprovalStatus
+    {
+
+        public string ApprovalStatus { get; set; }
+        public string ProductId { get; set; }
+        public string ReasonID { get; set; }
+
+
+    }
 }
